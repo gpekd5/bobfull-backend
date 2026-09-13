fc33f26d3b1f6874ff9526ab3038876077a89a4e
gpekd5 <gpekd5@naver.com>
2026-09-13 23:55:00 +0900

    test: Restaurant 이동 후 미사용 import 정리

--- src/test/java/com/bobfull/restaurant/restaurant/presentation/controller/RestaurantSearchCacheEffectInvestigationTest.java
+++ src/test/java/com/bobfull/restaurant/restaurant/presentation/controller/RestaurantSearchCacheEffectInvestigationTest.java
@@ -3,7 +3,6 @@ package com.bobfull.restaurant.restaurant.presentation.controller;
 import static org.assertj.core.api.Assertions.assertThat;
 
 import com.bobfull.restaurant.restaurant.infrastructure.cache.RestaurantSearchCacheStore;
-import com.bobfull.restaurant.restaurant.presentation.dto.RestaurantSearchRequest;
 import com.bobfull.restaurant.restaurant.presentation.dto.RestaurantUpdateRequest;
 import com.bobfull.restaurant.restaurant.domain.entity.Restaurant;
 import com.bobfull.restaurant.restaurant.infrastructure.repository.RestaurantRepository;
